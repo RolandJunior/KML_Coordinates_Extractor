c290fda2d401fc8f86bd4764cf7497587ede2e07
Roland B. Junior <roland.junior@outlook.com>
2020-05-14 11:47:55 -0300

    v0.1.1
    Improvements:
    * Added "if __name__ == '__main__':" structure.
    
    Bug fixes:
    * Removal of leading and trailing "\n" from coordinates tag, if present.

--- KML_coord.py
+++ KML_coord.py
@@ -3,18 +3,25 @@
 import xml.etree.ElementTree as ET
 import re
 
-# KML parse
-tree = ET.parse('input.kml')
-root = tree.getroot()
 
-# Identify default namespace
-namespace = re.match('\{(.*?)\}kml', root.tag).group(1)
-ns = {'def': namespace}
+def main():
+    # KML parse
+    tree = ET.parse('input.kml')
+    root = tree.getroot()
+    
+    # Identify default namespace
+    namespace = re.match('\{(.*?)\}kml', root.tag).group(1)
+    ns = {'def': namespace}
+    
+    # Create file with coordinates
+    f = open('output.txt','w')
+    for i in root.findall('.//def:Placemark[def:Point]', ns):
+      name = i.find('def:name', ns).text
+      coord = i.find('.//def:coordinates', ns).text
+      coord = coord.strip().split(',')
+      f.write(f'{name},{coord[1]},{coord[0]}\n')
+    f.close() 
 
-# Create file with coordinates
-f = open('output.txt','w')
-for i in root.findall('.//def:Placemark[def:Point]', ns):
-  name = i.find('def:name', ns).text
-  coord = i.find('.//def:coordinates', ns).text.split(',')
-  f.write(f'{name},{coord[1]},{coord[0]}\n')
-f.close() 
+
+if __name__ == '__main__':
+    main()
